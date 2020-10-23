511a40fde512f173984235dbb6f64421daf7555f
Donovan Mosley <dmosley28@gmail.com>
2020-10-23 14:54:16 -0400

    cors validation solution option 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
 //Navigation hamburger click function:
-//Use get elements by class...
-// const hamburger = document.querySelector(".menu-toggle")
-
-// hamburger.addEventListener("click", function(){
-//   // $('nav').toggleClass('active')
-//   console.log("Random Comment");
-// })
-
 const menuToggle = document.querySelector('.menu-toggle');
 
         const nav = document.querySelector('nav');
@@ -20,39 +12,66 @@ const menuToggle = document.querySelector('.menu-toggle');
         });
 
 
-// target.addEventListener(type, listener [, options]);
 
-// $(document).ready(function(){
-//   $('.menu-toggle').click(function(){
-//       $('nav').toggleClass('active')
-//   })
-// })
+//Using Fetch to call the Yelp API:
+// cors solution #1:
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer tXRkZsFn77Nq3R-Ucvl-G6Soe3J4b2rFX5rdS9GRYmZ4Hzas2-LjABrAx397enuHY-csf_5xC9cFWLiDxFMK1KlkYzgwXhP4qbB4sJe3C3brz7VURUe5d3m70HiRX3Yx");
+myHeaders.append("Cookie", "__cfduid=db290300ecfe95ec1fe3bc92c388c3c991586618117");
+myHeaders.append("Access-Control-Allow-Origin", "*");
 
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+var targetUrl = 'https://api.yelp.com/v3/businesses/search'
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+
+fetch(proxyUrl + targetUrl)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+
+fetch('https://api.yelp.com/v3/businesses/search')
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+
+//Read and parse an external file (such as JSON or CSV) into your application and display some data from that in your app
+
+
+
+//Countdown Timer:    
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 24, 2021 15:37:25").getTime();
+// var countDownDate = new Date("Jul 24, 2021 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// // Update the count down every 1 second
+// var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+//   // Get today's date and time
+//   var now = new Date().getTime();
     
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
     
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+//   // Output the result in an element with id="demo"
+//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
     
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+//   // If the count down is over, write some text 
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
