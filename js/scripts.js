e16435bde4c726376b3736ba9aa2349ef90899d8
donovanmosley <dmosley28@gmail.com>
2020-11-03 06:46:07 -0500

    stored each business object in variables, added if/else statement for found results to and none, added display for number of found results, styled found results with flexbox

--- js/scripts.js
+++ js/scripts.js
@@ -68,18 +68,51 @@ $(window).on("load", function() {
         "token_type": "Bearer"
       },
       success: function(data) {
-          console.log(data);
-          // Output the result in an element with id="searchOutput"
-          $('#searchResults').append(JSON.stringify(data))
-          // $('#searchResults').innerHTML = b.image_url + "Name: " + b.name + "Location: " + b.location + "Phone: "
-          // + b.display_phone + "Rating: " + b.rating + "Price: " + b.price;
-      },
-      error: function (data) {
-        console.error("Sorry, there was a problem!");
-      },
-    })
-  })
+        // Grab the results from the API JSON return
+        var totalresults = data.total;
+        // If our results are greater than 0, continue
+        if (totalresults > 0){
+            // Display a header on the page with the number of results
+            $('#found').append('<h5>We discovered ' + totalresults + ' results!</h5><br>');
+            // Itirate through the JSON array of 'businesses' which was returned by the API
+            $.each(data.businesses, function(i, item) {
+                // Store each business's object in a variable
+                var id = item.id;
+                var alias = item.alias;
+                var phone = item.display_phone;
+                var image = item.image_url;
+                var name = item.name;
+                var rating = item.rating;
+                var reviewcount = item.review_count;
+                var address = item.location.address1;
+                var city = item.location.city;
+                var state = item.location.state;
+                var zipcode = item.location.zip_code;
+                // Append our result into our page
+                $('#results').append('<div id="' + id + '" style="margin-top:20px;margin-bottom:20px;margin-left:40px;margin-right:40px;"><img src="' + image + '" style="width:200px;height:150px;"><br>Name: <b>' + name + '</b> <br> Address: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>Phone: ' + phone + '<br>Rating: ' + rating + ' with ' + reviewcount + ' reviews.</div>');
+          });
+        } else {
+            // If our results are 0; no businesses were returned by the JSON therefor we display on the page no results were found
+            $('#results').append('<h5>We discovered no results!</h5>');
+        }
+      }  
+    })  
+  })  
 })
+
+      //     console.log(data);
+      //     // Output the result in an element with id="searchOutput"
+      //     $('#searchResults').append(JSON.stringify(data))
+      //     // This never worked:
+      //     // $('#searchResults').innerHTML = b.image_url + "Name: " + b.name + "Location: " + b.location + "Phone: "
+      //     // + b.display_phone + "Rating: " + b.rating + "Price: " + b.price;
+      // },
+      // error: function (data) {
+      //   console.error("Sorry, there was a problem!");
+      // },
+    // })
+//   })
+// })
 
 
 
